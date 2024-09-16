0e4dab6b944207ddc4cd8b59be466286673498ba
CamiloGarcesGL <camilo.garces@gorillalogic.com>
2024-09-16 11:10:53 -0500

    fix connection

--- backend-vue-ai/index.js
+++ backend-vue-ai/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/vue_db/mis_skills', {
+mongoose.connect('mongodb://localhost:27017/db_vue', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
