7a7d8097be5f00ba5195c946137b4870bba61ad5
CamiloGarcesGL <camilo.garces@gorillalogic.com>
2024-09-16 14:01:12 -0500

    new method

--- src/components/CallGraphQL.vue
+++ src/components/CallGraphQL.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="classbutton">
       <input v-model="newName" type="text" placeholder=" New skill">
-      <button  class="classbuttonadded" type="button">
+      <button  class="classbuttonadded" type="button" @click="handleAddItem">
         <span>Add</span>
       </button>
     </div>
@@ -16,8 +16,13 @@
     <div class="contentbuttonskill">
 
       <ul>
-        <li v-for="name in dataResponse" :key="name">
+        <li v-for="name in dataResponse" :key="name._id">
           {{name.name}}
+          <button class="" type="button" @click="handleDeleteItem(name._id)">
+            <svg class="w-[15px] h-[15px] text-gray-800 dark:text-blue" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+              <path fill-rule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"/>
+            </svg>
+          </button>
         </li>
       </ul>
     </div>
@@ -29,6 +34,8 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:5000/api/mis_skills';
 const dataResponse = ref([]);
+const newName = ref('');
+const showModal = ref(false);
 
 const getItems = async () => {
   try {
@@ -49,15 +56,43 @@ const addItem = async (item) => {
   }
 };
 
+const handleAddItem = async () => {
+  if (!newName.value.trim()) {
+    showModal.value = true;
+    return;
+  }
+
+  const newItem = { name: newName.value.trim() };
+
+  const addedItem = await addItem(newItem);
+  if (addedItem) {
+    dataResponse.value.push(addedItem); 
+    newName.value = '';
+};
+}
+const deleteItem = async (id) => {
+  try {
+    const response = await axios.delete(`${API_URL}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error', error);
+  }
+};
+
+const handleDeleteItem = async (id) => {
+  const deletedItem = await deleteItem(id);
+  if (deletedItem) {
+    dataResponse.value = dataResponse.value.filter(item => item._id !== id);
+  }
+};
+
 onMounted(() => {
   getItems();
 });
 
-
 </script>
 
 <style>
-
 .modal {
   position: fixed;
   top: 0;
