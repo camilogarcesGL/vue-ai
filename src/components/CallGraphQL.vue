72c9a5b0e88c729911f1f1357b77418ad5a22806
CamiloGarcesGL <camilo.garces@gorillalogic.com>
2024-09-17 13:30:43 -0500

    create logic graphql

--- src/components/CallGraphQL.vue
+++ src/components/CallGraphQL.vue
@@ -14,11 +14,10 @@
     </div>
 
     <div class="contentbuttonskill">
-
       <ul>
-        <li v-for="name in dataResponse" :key="name._id">
-          {{name.name}}
-          <button class="" type="button" @click="handleDeleteItem(name._id)">
+        <li v-for="item in dataResponse" :key="item.id">
+          {{item.name}}
+          <button class="" type="button" @click="handleDeleteItem(item.id)">
             <svg class="w-[15px] h-[15px] text-gray-800 dark:text-blue" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
               <path fill-rule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"/>
             </svg>
@@ -28,69 +27,93 @@
     </div>
   </template>
   
-<script setup>
-import { ref, onMounted }    from 'vue';
-import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/api/mis_skills';
-const dataResponse = ref([]);
-const newName = ref('');
-const showModal = ref(false);
-
-const getItems = async () => {
-  try {
-    const response = await axios.get(API_URL);
-    dataResponse.value = response.data;
-    return response.data;
-  } catch (error) {
-    console.error('Error', error);
-  }
-};
-
-const addItem = async (item) => {
-  try {
-    const response = await axios.post(API_URL, item);
-    return response.data;
-  } catch (error) {
-    console.error('Error', error);
-  }
-};
-
-const handleAddItem = async () => {
-  if (!newName.value.trim()) {
-    showModal.value = true;
-    return;
-  }
-
-  const newItem = { name: newName.value.trim() };
-
-  const addedItem = await addItem(newItem);
-  if (addedItem) {
-    dataResponse.value.push(addedItem); 
-    newName.value = '';
-};
-}
-const deleteItem = async (id) => {
-  try {
-    const response = await axios.delete(`${API_URL}/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error', error);
-  }
-};
-
-const handleDeleteItem = async (id) => {
-  const deletedItem = await deleteItem(id);
-  if (deletedItem) {
-    dataResponse.value = dataResponse.value.filter(item => item._id !== id);
-  }
-};
-
-onMounted(() => {
-  getItems();
-});
-
-</script>
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios';
+  
+  const API_URL = 'http://localhost:5000/graphql';
+  const dataResponse = ref([]);
+  const newName = ref('');
+  const showModal = ref(false);
+  
+  const getItems = async () => {
+    try {
+      const query = `
+        query {
+          getSkills {
+            id
+            name
+            description
+          }
+        }
+      `;
+      const response = await axios.post(API_URL, { query });
+      dataResponse.value = response.data.data.getSkills;
+    } catch (error) {
+      console.error('Error', error);
+    }
+  };
+  
+  const addItem = async (item) => {
+    try {
+      const mutation = `
+        mutation {
+          addSkill(name: "${item.name}", description: "${item.description || ''}") {
+            id
+            name
+            description
+          }
+        }
+      `;
+      const response = await axios.post(API_URL, { query: mutation });
+      return response.data.data.addSkill;
+    } catch (error) {
+      console.error('Error', error);
+    }
+  };
+  
+  const handleAddItem = async () => {
+    if (!newName.value.trim()) {
+      showModal.value = true;
+      return;
+    }
+  
+    const newItem = { name: newName.value.trim() };
+  
+    const addedItem = await addItem(newItem);
+    if (addedItem) {
+      dataResponse.value.push(addedItem); 
+      newName.value = '';
+    }
+  };
+  
+  const deleteItem = async (id) => {
+    try {
+      const mutation = `
+        mutation {
+          deleteSkill(id: "${id}") {
+            id
+          }
+        }
+      `;
+      const response = await axios.post(API_URL, { query: mutation });
+      return response.data.data.deleteSkill;
+    } catch (error) {
+      console.error('Error', error);
+    }
+  };
+  
+  const handleDeleteItem = async (id) => {
+    const deletedItem = await deleteItem(id);
+    if (deletedItem) {
+      dataResponse.value = dataResponse.value.filter(item => item.id !== id);
+    }
+  };
+  
+  onMounted(() => {
+    getItems();
+  });
+  </script>
 
 <style>
 .modal {
