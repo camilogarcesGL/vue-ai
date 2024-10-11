a32c2876efe7beed779271d2a3551d0d0aa2659f
CamiloGarcesGL <camilo.garces@gorillalogic.com>
2024-10-11 15:40:30 -0500

    create base login

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import Navigation from './components/Navigation.vue'
+import login from './components/login.vue'
 import './style.css'
 </script>
 
 <template>
-  <Navigation />
+  <login />
+
+<!-- <Navigation /> -->   
 </template>
---
+++ src/components/login.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { defineProps, ref } from 'vue'
+
+const props = defineProps({ 
+    name: {
+        type: String,
+    },
+})
+
+const inputName = ref<string>("example 1") //null
+const inputPassword = ref<string>("example 2") //null
+const handleLogin = () => {
+    console.log(inputName, inputPassword)
+}
+</script>
+
+<template>
+    <div class="contentLogin">
+        <div class="login">
+            <h2>Please Authenticated</h2>
+            <label>Name:</label>
+            <input class= "input-with-padding" type="text" v-model="inputName" />
+            <label>Password:</label>
+            <input class="input-with-padding" type="password"  v-model="inputPassword" />
+            <button  class="classbuttonadded" type="button" @click="handleLogin">Login</button>
+        </div>
+        <div>
+     
+        </div>
+    </div>
+
+</template>
